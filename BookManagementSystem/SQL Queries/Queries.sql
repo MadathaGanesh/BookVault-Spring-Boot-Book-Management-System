15735e5dbaeaa4f059482e83eaee9b1de51849f6
MadathaGanesh <ganeshmadatha159@gmail.com>
2025-11-04 18:16:10 +0530

    Added MySQL Workbench SQL queries for database setup

---
+++ BookManagementSystem/SQL Queries/Queries.sql
@@ -0,0 +1,9 @@
+use demo;
+
+select * from library;
+
+select * from library where bookid =7;
+
+select * from library where price between 290 and 400;
+
+select * from library where book_name like 'T%';
